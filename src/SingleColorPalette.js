ad5f3452604b7fae20a578d0016e61aab245c994
Aigars Peda <aigarspeda@gmail.com>
2019-07-24 09:16:18 +0300

    Add GoBack Link

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,55 +1,55 @@
 import React, { Component } from 'react'
-import ColorBox from './ColorBox'
+import { Link } from 'react-router-dom'
 import Navbar from './Navbar'
 import PaletteFooter from './PaletteFooter'
-
+import ColorBox from './ColorBox'
 
 class SingleColorPalette extends Component {
   constructor(props) {
     super(props)
-    this.state = { format: 'hex' }
     this._shades = this.gatherShades(this.props.palette, this.props.colorId)
+    this.state = { format: 'hex' }
     this.changeFormat = this.changeFormat.bind(this)
-    // console.log(this._shades)
   }
   gatherShades(palette, colorToFilterBy) {
     let shades = []
     let allColors = palette.colors
-    
-    for(let key in allColors) {
+
+    for (let key in allColors) {
       shades = shades.concat(
         allColors[key].filter(color => color.id === colorToFilterBy)
       )
     }
-    return shades
+    return shades.slice(1)
   }
-  changeFormat(val){
+  changeFormat(val) {
     this.setState({ format: val })
   }
   render() {
-    const { format } = this.state
-    const { paletteName, emoji } = this.props.palette
+    const { format } = this.state;
+    const { paletteName, emoji, id } = this.props.palette
     const colorBoxes = this._shades.map(color => (
-      <ColorBox key={color.name}
-                name={color.name}
-                background={color[format]}
-                showLink={false}
+      <ColorBox
+        key={color.name}
+        name={color.name}
+        background={color[format]}
+        showLink={false}
       />
-    ))
+    ));
     return (
-      <div className='Palette'>
-        <Navbar handleChange={this.changeFormat} 
-                showingAllColors={false}
-        />
-        <div className='Palette-colors' >
+      <div className='SingleColorPalette Palette'>
+        <Navbar handleChange={this.changeFormat} showingAllColors={false} />
+        <div className='Palette-colors'>
           {colorBoxes}
+          <div className='go-back ColorBox'>
+            <Link to={`/palette/${id}`} className='back-button'>
+              GO BACK
+            </Link>
+          </div>
         </div>
-        <PaletteFooter paletteName={ paletteName } 
-                       emoji={ emoji }
-        />
+        <PaletteFooter paletteName={paletteName} emoji={emoji} />
       </div>
     )
   }
 }
-
 export default SingleColorPalette
