e4e8ceb4e6bb7d41c4cd230211294293d9e2704d
Aigars <aigarspeda@gmail.com>
2019-08-06 14:40:55 +0300

    Update NewPaletteForm.js

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -10,8 +10,7 @@ import Divider from '@material-ui/core/Divider'
 import IconButton from '@material-ui/core/IconButton'
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
 import DraggableColorList from './DraggableColorList'
-
-import  arrayMove  from 'array-move'
+import arrayMove from 'array-move'
 import PaletteFormNav from './PaletteFormNav'
 
 const drawerWidth = 400
