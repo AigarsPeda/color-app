28a2bb48eeb516f5046f827ca1243f516ecb05d5
Aigars <aigarspeda@gmail.com>
2019-08-13 09:14:51 +0300

    Make form and navbar responsive

--- src/styles/NavbarStyles.js
+++ src/styles/NavbarStyles.js
@@ -1,47 +1,55 @@
+import sizes from "./sizes";
+
 export default {
   Navbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    height: '8vh'
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    height: "8vh"
   },
   Logo: {
-    marginRight: '15px',
-    padding: '0 13px',
-    fontSize: '22px',
-    backgroundColor: '#eceff1',
-    fontFamily: 'Roboto',
-    height: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    '& a': {
-      textDecoration: 'none',
-      color: 'black'
+    marginRight: "15px",
+    padding: "0 13px",
+    fontSize: "22px",
+    backgroundColor: "#eceff1",
+    fontFamily: "Roboto",
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    "& a": {
+      textDecoration: "none",
+      color: "black"
+    },
+    [sizes.down("xs")]: {
+      display: "none"
+    }
+  },
+  Slider: {
+    width: "340px",
+    margin: "0 10px",
+    display: "inline-block",
+    "& .rc-slider-handle": {
+      height: "8px"
+    },
+    "& .rc-slider-track": {
+      backgroundColor: "transparent"
+    },
+    "& .rc-slider-handle, .rc-slider-handle:active, .rc-slider-handle:focus, .rc-slider-handle:hover": {
+      backgroundColor: "green",
+      outline: "none",
+      border: "2px solid green",
+      boxShadow: "none",
+      width: "13px",
+      height: "13px",
+      marginLeft: "-7px",
+      marginTop: "-3px"
+    },
+    [sizes.down("md")]: {
+      width: "150px"
     }
   },
   SelectContainer: {
-    marginLeft: 'auto',
-    marginRight: '1rem'
-  },
-  Slider: {
-    width: '340px',
-    margin: '0 10px',
-    display: 'inline-block',
-    '& .rc-slider-handle': {
-      height: '8px'
-    },
-    '& .rc-slider-track': {
-      backgroundColor: 'transparent'
-    },
-    '& .rc-slider-handle, .rc-slider-handle:active, .rc-slider-handle:focus, .rc-slider-handle:hover': {
-      backgroundColor: 'green',
-      outline: 'none',
-      border: '2px solid green',
-      boxShadow: 'none',
-      width: '13px',
-      height: '13px',
-      marginLeft: '-7px',
-      marginTop: '-3px'
-    }
+    marginLeft: "auto",
+    marginRight: "1rem"
   }
-}
+};
